5a2f8337fb5ef09885c737971b7f182cd7d2fc22
Lance Satorre <lanceerrotas@gmail.com>
2025-10-22 20:40:11 +0800

    feat: MidtermExam

---
+++ PYTHON_PROGRAMMING/MidtermExam-Satorre/helpers/math_utils.py
@@ -0,0 +1,5 @@
+def area(length, width):
+    return length * width
+
+def area_improved(length: float, width: float) -> float:
+    return length * width
---
+++ PYTHON_PROGRAMMING/MidtermExam-Satorre/helpers/string_utils.py
@@ -0,0 +1,5 @@
+def shout(s):
+    return s.upper()
+
+def shout_improved(s: str) -> str:
+    return s.upper()
